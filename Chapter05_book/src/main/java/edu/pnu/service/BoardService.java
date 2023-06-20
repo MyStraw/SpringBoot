de1205c0cf74ba674f2ffacdd6609574bdb8f421
MyStraw <kikongdosa@gmail.com>
2023-06-21 00:32:29 +0900

    .gitignore 반영

---
+++ Chapter05_book/src/main/java/edu/pnu/service/BoardService.java
@@ -0,0 +1,17 @@
+package edu.pnu.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.pnu.persistence.BoardRepository;
+
+//@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	
+	
+
+
+}

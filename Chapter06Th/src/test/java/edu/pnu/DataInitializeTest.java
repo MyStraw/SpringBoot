fcdb5795880ae8a3a45d77eedb6996a811ad9cf9
MyStraw <kikongdosa@gmail.com>
2023-06-29 02:27:59 +0900

    백업

--- Chapter06Th/src/test/java/edu/pnu/DataInitializeTest.java
+++ Chapter06Th/src/test/java/edu/pnu/DataInitializeTest.java
@@ -1,10 +1,8 @@
 package edu.pnu;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.pnu.domain.Board;
 import edu.pnu.domain.Member;
